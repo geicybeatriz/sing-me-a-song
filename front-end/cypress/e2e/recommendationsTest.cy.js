2eb198b89ee04eb77a826e8a6dea244b7f95f7ca
Geicy Beatriz Soudré Cardoso <geicybeatriz@gmail.com>
2022-08-01 05:10:37 -0300

    test: add e2e create new recommendation test

--- front-end/cypress/e2e/recommendationsTest.cy.js
+++ front-end/cypress/e2e/recommendationsTest.cy.js
@@ -1,6 +1,8 @@
 /// <reference types="cypress" />
+import { faker } from "@faker-js/faker";
 
 const URL = "http://localhost:3000";
+const URL_API = "http://localhost:5000"
 
 beforeEach(() => {
     cy.resetDatabase();
@@ -11,6 +13,17 @@ describe("home page", () => {
         cy.visit("/");
         cy.url().should("equal", `${URL}/`);
         cy.get("input").should("be.visible");
-        
-    })
+    });
+    
+    it("should post new recommendation", () => {
+        cy.visit("/");
+        for(let i = 0; i < 3; i++){
+            const recommendation = {
+                name: faker.lorem.words(3),
+                youtubeLink: "https://youtu.be/perTTMRpc_U"
+            }
+            cy.createRecommendations(recommendation);
+            cy.contains(recommendation.name);
+        }
+    });
 })
--- front-end/cypress/support/commands.js
+++ front-end/cypress/support/commands.js
@@ -1,8 +1,15 @@
 const URL_API = "http://localhost:5000";
 
 Cypress.Commands.add("resetDatabase", () => {
-    cy.log("reset database")
     cy.request("DELETE", `${URL_API}/e2e/reset`).then( res => {
         cy.log(res)
     })
-})
+});
+
+Cypress.Commands.add("createRecommendations", (recommendation) => {
+    cy.get("input[placeholder='Name']").type(recommendation.name);
+    cy.get("input[placeholder='https://youtu.be/...']").type(recommendation.youtubeLink);
+    cy.intercept("POST", `${URL_API}/recommendations`).as("postRecommendation");
+    cy.get("button").click();
+    cy.wait("@postRecommendation");
+});
